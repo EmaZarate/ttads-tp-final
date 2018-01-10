dd0c59c88a54f60a2569192696ecafcb739a2d53
Emanuel <ema.zarate90@gmail.com>
2018-01-10 12:29:07 -0300

    abm user con permisos

--- BackEnd/app/routes/user.js
+++ BackEnd/app/routes/user.js
@@ -4,46 +4,85 @@ const bodyParser = require('body-parser');
 var userModel = mongoose.model('user');
 var permitsModel = mongoose.model('permits');
 
-router.get('/:_id', (req,res) => {
-  userModel.find({_id:req.params._id}) 
-  .populate('permits')
-  .then(user=>{
-    name = user.name
-    console.log(name)
-    return res.send(user.name)
-  })
-  
+router.get('/:id', (req,res) => {
+    userModel.find({_id:req.params.id}) 
+   .populate('permits')
+   .then(user=>{
+      if(user[0].permits.type==="administrador"){
+        userModel.find({})
+        .then(users=>{
+          return res.json(users)
+        })
+       }
+     else{
+       return res.json({permiso:'no tiene permiso'})
+      }
+    }) 
 });
 
-router.get('/:name', (req,res) =>{
-    let name=req.params.name;
-    userModel.find({ name:name})
-    .then( client =>{
-      if(!user){return res.sendStatus(404);}
-      return res.json(user)
-    });
+router.get('/:id_admin/:name', (req,res) =>{
+  userModel.find({_id:req.params.id_admin}) 
+   .populate('permits')
+   .then(user=>{
+      if(!user){return res.sendStatus(404) }
+      else if(user[0].permits.type==="administrador"){
+        userModel.find({name:req.params.name})
+        .then(users=>{
+          if(!users) { return res.json('usuario no encontrado') }
+          else{ return res.json(users) }
+        })
+       }
+     else{
+       return res.json({permiso:'no tiene permiso'})
+      }
+    }) 
 });
 
 router.get('/:name/:surname', (req,res) =>{
-    let name=req.params.name;
-    let surname = req.params.surname;
-    user.find({ name:name, surname:surname})
-    .then( user =>{
-      if(!user){return res.sendStatus(404);}
-      return res.json(user)
-    });
+  userModel.find({_id:req.params.id_admin}) 
+  .populate('permits')
+  .then(user=>{
+     if(!user){return res.sendStatus(404) }
+     else if(user[0].permits.type==="administrador"){
+       userModel.find({name:req.params.name,surname:req.params.surname})
+       .then(users=>{
+         if(!users) { return res.json('usuario no encontrado') }
+         else{ return res.json(users) }
+       })
+      }
+    else{
+      return res.json({permiso:'no tiene permiso'})
+     }
+   }) 
 });
 
 
-router.post('/',(req,res)=>{
-  let instUser = new userModel(req.body);
-  instUser.save()
-  .then(user => {
-    if(!user){return res.sendStatus(404);}
-    else{
-        return res.json(user);
+router.post('/:id',(req,res)=>{
+  userModel.find({_id:req.params.id}) 
+  .populate('permits')
+  .then(user=>{
+     if(user[0].permits.type==="administrador"){
+       permitsModel.find({type:req.body.typePermits})
+       .then(permits=>{
+         let instUser = new userModel();
+         instUser.name = req.body.name;
+         instUser.surname = req.body.surname;
+         instUser.phone = req.body.phone;
+         instUser.email = req.body.email;
+         instUser.address = req.body.address;
+         instUser.password = req.body.password;
+         instUser.permits = permits._id
+         instUser.save()
+         .then(user => {
+           if(!user) { return res.sendStatus(404) }
+           else { return res.sendStatus(200)}
+          })
+       })  
       }
-  });
+     else{
+       return res.json({permiso:'no tiene permiso'})
+      }
+  }) 
 });
 
 router.put('/:_id',(req,res)=>{
